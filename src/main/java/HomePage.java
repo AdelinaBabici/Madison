a7c20333a76f6c88b03ea08abd009a4d48b4825b
Adelina Babici <adelinababici@yahoo.com>
2021-07-28 15:08:11 +0300

    Initial commit

---
+++ src/main/java/HomePage.java
@@ -0,0 +1,82 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class HomePage {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+
+        //Create a new webdriver instance
+        WebDriver driver = new ChromeDriver();
+
+        //Launch a new browser session
+        driver.get("http://qa2.dev.evozon.com/");
+
+        System.out.println("Page title is: " + driver.getTitle());
+        System.out.println("Current URL is: " + driver.getCurrentUrl());
+
+        WebElement logo = driver.findElement(By.cssSelector("#header > div > a > img.large"));
+        logo.click();
+
+        WebElement accessories = driver.findElement(By.cssSelector("#nav > ol > li.level0.nav-3.parent > a"));
+        accessories.click();
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+        driver.quit();
+//
+
+        WebDriver driver1 = new ChromeDriver();
+        driver1.get("http://qa2.dev.evozon.com/");
+
+        WebElement account = driver1.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label"));
+        account.click();
+        driver1.quit();
+//
+        WebDriver driver2 = new ChromeDriver();
+        driver2.get("http://qa2.dev.evozon.com/");
+        Select select = new Select(driver2.findElement(By.cssSelector("#select-language")));
+        List<WebElement> languages = select.getOptions();
+        System.out.println(languages.size());
+        select.selectByIndex(1);
+        driver2.quit();
+
+        //
+
+        WebDriver driver3 = new ChromeDriver();
+        driver3.get("http://qa2.dev.evozon.com/");
+        WebElement search = driver3.findElement(By.cssSelector("#search"));
+        search.clear();
+        search.sendKeys("top");
+        search.submit();
+        driver3.quit();
+
+        WebDriver driver4 = new ChromeDriver();
+        driver4.get("http://qa2.dev.evozon.com/");
+
+        List<WebElement> products = driver4.findElements(By.cssSelector("body > div > div.page > div.main-container.col1-layout > div > div > div.std > div.widget.widget-new-products > div.widget-products > ul > li > div > h3"));
+        int numOfChildren = products.size();
+        System.out.println(numOfChildren);
+        for(WebElement element : products) {
+            System.out.println(element.getText());
+        }
+        driver4.quit();
+
+
+        WebDriver driver5 = new ChromeDriver();
+        driver5.get("http://qa2.dev.evozon.com/");
+        List<WebElement> headline = driver5.findElements(By.cssSelector(("#nav > ol")));
+        for(WebElement options : headline){
+            System.out.println(options.getText());
+        }
+        WebElement sale = driver5.findElement(By.cssSelector("#nav > ol > li.level0.nav-5.parent"));
+        sale.click();
+        driver5.quit();
+
+    }
+}
